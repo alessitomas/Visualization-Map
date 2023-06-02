ae56324b34c2d85ebd75c6d98fbc59db6dd2fd68
esdrasgc <esdrasgcdev@gmail.com>
2023-06-02 15:05:44 -0300

    popup clicavel adcionado a polyline

--- visualization-map/src/pages/MapPage/MapPage.jsx
+++ visualization-map/src/pages/MapPage/MapPage.jsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
+import { MapContainer, TileLayer, Polyline, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import polyline from '@mapbox/polyline';
 
 const MapPage = () => {
   const [poly, setPoly] = useState([]);
+  const [popupInfo, setPopupInfo] = useState(null);
+
+  const decodePolyline = (encodedPolyline) => {
+    const decodedPolyline = polyline.decode(encodedPolyline);
+    const points = decodedPolyline.map((point) => [point[0], point[1]]);
+    return points;
+  };
 
   useEffect(() => {
     const fetchPolylineData = async () => {
@@ -12,17 +19,24 @@ const MapPage = () => {
         const response = await fetch('http://localhost:5000/rota');
         const data = await response.json();
 
-        const decodePolyline = (encodedPolyline) => {
-          const decodedPolyline = polyline.decode(encodedPolyline);
-          const points = decodedPolyline.map((point) => ({ lat: point[0], lng: point[1] }));
-          return points;
-        };
         const polylines = data.rotas.map((rota) => {
           const polylineData = decodePolyline(rota.encodedRoutes);
-          return <Polyline key={rota.id} pathOptions={{ color: `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})` }} positions={polylineData} />;
+          return (
+            <Polyline 
+              key={rota.id} 
+              pathOptions={{ color: `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})` }} 
+              positions={polylineData} 
+              eventHandlers={{
+                click: (e) => {
+                  setPopupInfo({ position: e.latlng, data: { distanceMeters: rota.distanceMeters, duration: rota.duration }});
+                }
+              }}
+            />
+          );
         });
 
         setPoly(polylines);
+
       } catch (error) {
         console.error('Error fetching polyline data:', error);
       }
@@ -40,6 +54,12 @@ const MapPage = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {poly}
+      {popupInfo && (
+        <Popup position={popupInfo.position}>
+          <p>{popupInfo.data.distanceMeters} m</p>
+          <p>{popupInfo.data.duration}</p> 
+        </Popup>
+      )}
     </MapContainer>
   );
 }
