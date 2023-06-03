9ee65cd92ac5cee7da3266df2b849497bf431d45
esdrasgc <esdrasgcdev@gmail.com>
2023-06-03 17:20:12 -0300

    alteracoes necessarias para enviar zoom coord para o back

--- visualization-map/src/pages/MapPage/MapPage.jsx
+++ visualization-map/src/pages/MapPage/MapPage.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState, useMemo } from 'react';
-import { MapContainer, TileLayer, Polyline, Popup, Polygon, Marker } from 'react-leaflet';
+import { useEffect, useState, useRef, useMemo } from 'react';
+import { MapContainer, useMap, TileLayer, Polyline, Popup, Polygon, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import polyline from '@mapbox/polyline';
 
@@ -8,13 +8,42 @@ const MapPage = () => {
   const [popupInfo, setPopupInfo] = useState(null);
   const [gon, setGon] = useState([]);
   const [markers, setMarkers] = useState([]);
+  const [mapCenter, setMapCenter] = useState([-23.513860, -46.597593]);
+  const [mapZoom, setMapZoom] = useState(13);
+  const mapRef = useRef();
 
   const decodePolyline = (encodedPolyline) => {
     const decodedPolyline = polyline.decode(encodedPolyline);
     return decodedPolyline.map((point) => ({ lat: point[0], lng: point[1] }));
   };
 
+  const MapEvents = () => {
+    const map = useMap();
+
+    useEffect(() => {
+      mapRef.current = map;
+    }, [map]);
+
+    useEffect(() => {
+      if (!mapRef.current) return;
+
+      const handleMove = () => {
+        setMapCenter(mapRef.current.getCenter());
+        setMapZoom(mapRef.current.getZoom());
+      };
+
+      mapRef.current.on('moveend', handleMove);
+
+      return () => {
+        mapRef.current.off('moveend', handleMove);
+      };
+    }, [mapRef]);
+
+    return null;
+  };
+
   useEffect(() => {
+
     const fetchPolylineData = async () => {
       try {
         const response = await fetch('http://localhost:5000/rota');
@@ -38,7 +67,7 @@ const MapPage = () => {
 
     fetchPolylineData();
     fetchAreaData();
-  }, []);
+  }, [mapCenter, mapZoom]);
 
   const polylines = useMemo(() => poly.map((rota) => {
     const polylineData = decodePolyline(rota.encodedRoutes);
@@ -62,41 +91,41 @@ const MapPage = () => {
         Origem<br />
         Lat: {rota.latitudeOrigem}<br />
         Lng: {rota.longitudeOrigem}<br />
-      </Popup>
+        </Popup>
     </Marker>,
     <Marker key={`destination-${rota.id}`} position={[rota.latitudeDestino, rota.longitudeDestino]}>
-      <Popup>
+    <Popup>
         Destino<br />
         Lat: {rota.latitudeDestino}<br />
         Lng: {rota.longitudeDestino}<br />
-      </Popup>
+    </Popup>
     </Marker>
-  ])), [markers]);
 
-  const polygons = useMemo(() => gon.map((area) => {
+    ])), [markers]);
+
+    const polygons = useMemo(() => gon.map((area) => {
     const positions = area.coords.map(coord => [coord[1], coord[0]]);
     return <Polygon key={area.name} pathOptions={{ color: 'blue' }} positions={positions} />;
-  }), [gon]);
+    }), [gon]);
 
-  const center = [-23.513860, -46.597593];
-
-  return (
-    <MapContainer center={center} zoom={13} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {polylines}
-      {popupInfo && (
+    return (
+        <MapContainer center={mapCenter} zoom={mapZoom} scrollWheelZoom={true}>
+        <MapEvents />
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {polylines}
+        {popupInfo && (
         <Popup position={popupInfo.position}>
-          Distancia: {popupInfo.data.distanceMeters} m <br />
-          Tempo: {popupInfo.data.duration}
+        Distancia: {popupInfo.data.distanceMeters} m <br />
+        Tempo: {popupInfo.data.duration}
         </Popup>
-      )}
-      {polygons}
-      {markersElements}
-    </MapContainer>
-  );
+        )}
+        {polygons}
+        {markersElements}
+        </MapContainer>
+    );
 };
 
 export default MapPage;
